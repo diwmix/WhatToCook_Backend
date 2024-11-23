5e972f7bc1bfe6aea7d2a32872e5cfbc1f6b9513
OleksandrKuchera <leato4ek@gmail.com>
2024-11-23 16:48:06 +0200

    50\50 goto login\regist and gotovo profiles

--- WhatToCook/recipe/models.py
+++ WhatToCook/recipe/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-from profiles.models import CustomUser
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class Recipe(models.Model):
+
     title = models.CharField(max_length=255)
     ingredients = models.TextField()
     instructions = models.TextField()
     category = models.CharField(max_length=100)
     subcategory = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='recipes/photos/') # to do cloudinary
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
