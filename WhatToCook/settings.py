fed88895d56ea8c03788572c26b9d5f846111aa0
OleksandrKuchera <leato4ek@gmail.com>
2025-01-08 22:27:31 +0200

    req

--- WhatToCook/settings.py
+++ WhatToCook/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['whattocook-backend.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = os.getenv('HOSTS', '').split(',')
 
 
 # Application definition
